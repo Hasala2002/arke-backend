6c758d6272d77c4a127f8d6990776ff82ddd40b2
Hasala2002 <hasala.h@hotmail.com>
2022-12-23 16:40:34 -0600

    pocketbase implementation

--- index.js
+++ index.js
@@ -1,50 +1,121 @@
-const express = require('express');
 
+import express from 'express'
 const app = express();
 
-const server = require('http').Server(app)
-// const io = require('socket.io')(server)
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
-const io = require('socket.io')(server, {cors: {origin: "*"}});
-const {v4: uuidv4 } = require('uuid')
-const { ExpressPeerServer } = require('peer')
-const peerServer = ExpressPeerServer(server,{
-    debug: true
-})
-const cors = require('cors')
+export const client = {
+  user: process.env.pocketbase_user,
+  password: process.env.pocketbase_password,
+  cors_link: process.env.react_client
+}
 
-app.use(cors())
-app.use('/peerjs',peerServer)
+import http from 'http'
 
+import { Server } from 'socket.io'
 
-// Set up a route to handle incoming requests
-app.get('/', (req, res) => {
-  // Send a response to the client
-  res.send('Hello, World!');
+import cors from 'cors'
+
+import PocketBase from 'pocketbase'
+import console from 'console';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+const authData = await pb.admins.authWithPassword(client.user, client.password);
+
+console.log(pb.authStore.isValid);
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: client.cors_link,
+    methods: ["GET", "POST"],
+  },
 });
+  
+app.get('/', (req, res)=>{
+  
+  res.send("<h1>Hello World! This is Arkē</h1>")
 
-io.on('connection',socket=>{
+})
 
-    // console.log("someone connected")
+  io.on('connection', (socket) => {
 
-    socket.on('join-room', (roomId, userData) => {
-        socket.join(roomId)
-        // console.log(roomId,userData)
-        io.to(roomId).emit('user-connected')
+    socket.on('check-if-room-exists', async (roomId) => {
 
-        socket.on('send-message',(messageData)=>{
-            // console.log(messageData)
-            io.to(roomId).emit('receive-message',messageData)
-        })
+      try {
+        const record = await pb.collection('rooms').getFirstListItem(`roomId="${roomId}"`)
+        const {roomName,participants} = record
+        socket.emit('room-data',{roomName,participants}) 
+      } catch (error) {
+        // console.log(error.data.code)
+        if(error.data.code === 404){
+          socket.emit('room-404',{code: 404})
+        }
+      }      
+
     })
 
-    socket.on('disconnect', () => {
-        // socket.to(roomId).emit('user-disconnected', userId, uname)
-        // console.log("someone disconnected")
-      })
-})
+    socket.on('join-room', async (roomId,userData) => {
+      socket.join(roomId);
 
-// Start the server on a specific port
-server.listen(process.env.PORT||3000, ()=> {
-    console.log('Server started successfully at PORT 3000!')
-  })
+              const {roomName, newRoom  } = userData
+
+
+
+              if(newRoom){  
+                const newRecord = await pb.collection('rooms').create({
+                  roomId: roomId,
+                  roomName: roomName,
+                  participants: 1
+                });
+              }else{
+                const record = await pb.collection('rooms').getFirstListItem(`roomId="${roomId}"`)
+                const {id} = record
+
+                const updateRecord = await pb.collection('rooms').update(id,{
+                  roomId: roomId,
+                  roomName: roomName,
+                  participants: io.sockets.adapter.rooms.get(roomId).size
+                })
+                io.to(roomId).emit('update-room-count',io.sockets.adapter.rooms.get(roomId).size)
+                // console.log(io.sockets.adapter.rooms.get(roomId).size)
+              }
+
+              io.to(roomId).emit('user-connected')
+
+              socket.on('send-message',(messageData)=>{
+                  // console.log(messageData)
+                  io.to(roomId).emit('receive-message',messageData)
+              })
+
+              socket.on('disconnect', async ()=>{
+                if(io.sockets.adapter.rooms.get(roomId)){
+                  const record = await pb.collection('rooms').getFirstListItem(`roomId="${roomId}"`)
+                  const {id} = record
+
+                  const updateRecord = await pb.collection('rooms').update(id,{
+                    roomId: roomId,
+                    roomName: roomName,
+                    participants: io.sockets.adapter.rooms.get(roomId).size
+                  })
+
+                  io.to(roomId).emit('update-room-count',io.sockets.adapter.rooms.get(roomId).size)
+
+                }else{
+                  const record = await pb.collection('rooms').getFirstListItem(`roomId="${roomId}"`)
+                  const {id} = record
+
+                  const deleteRecord = await pb.collection('rooms').delete(id)
+                }
+              })
+    });
+  });
+  
+  server.listen(3000, () => {
+    console.log('Socket.io server listening on port 3000');
+  });
