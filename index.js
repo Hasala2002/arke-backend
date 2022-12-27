0d950a45193710b1402af0fa6bfad7c193bedbe0
Hasala2002 <hasala.h@hotmail.com>
2022-12-26 23:18:30 -0600

    room leave function

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const app = express();
 import * as dotenv from 'dotenv' 
 dotenv.config()
 
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 export const client = {
   user: process.env.pocketbase_user,
   password: process.env.pocketbase_password,
 }
 
 import http from 'http'
+import https from 'https'
 
 import { Server } from 'socket.io'
 
@@ -27,16 +37,18 @@ console.log(pb.authStore.isValid);
 
 app.use(cors());
 
-// const httpsOptions = {
-//   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//   cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-// }
+//  const httpsOptions = {
+//    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+//  }
 
-const server = http.createServer(app);
+// const server = https.createServer(httpsOptions,app);
+
+const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173','https://arke.vercel.app'],
+    origin: ['http://localhost:5173','https://arke.vercel.app','https://app.arkechat.live'],
     methods: ["GET", "POST"],
   },
 });
@@ -97,7 +109,8 @@ app.get('/', (req, res)=>{
                   io.to(roomId).emit('receive-message',messageData)
               })
 
-              socket.on('disconnect', async ()=>{
+              socket.on('leave-room',async (roomId)=>{
+                socket.leave(roomId);
                 if(io.sockets.adapter.rooms.get(roomId)){
                   const record = await pb.collection('rooms').getFirstListItem(`roomId="${roomId}"`)
                   const {id} = record
@@ -115,6 +128,35 @@ app.get('/', (req, res)=>{
                   const {id} = record
 
                   const deleteRecord = await pb.collection('rooms').delete(id)
+                }
+              })
+
+              socket.on('disconnect', async ()=>{
+                if(io.sockets.adapter.rooms.get(roomId)){
+                  const record = await pb.collection('rooms').getFirstListItem(`roomId="${roomId}"`)
+                  const {id} = record
+
+                  const updateRecord = await pb.collection('rooms').update(id,{
+                    roomId: roomId,
+                    roomName: roomName,
+                    participants: io.sockets.adapter.rooms.get(roomId).size
+                  })
+
+                  io.to(roomId).emit('update-room-count',io.sockets.adapter.rooms.get(roomId).size)
+
+                }else{
+
+                  try{
+                    const record = await pb.collection('rooms').getFirstListItem(`roomId="${roomId}"`)
+                    const {id} = record
+  
+                    const deleteRecord = await pb.collection('rooms').delete(id)
+                  }catch(error){
+                    if(error.data.code === 404){
+                      console.log("Room already deleted!")
+                    }
+                  }
+
                 }
               })
     });
