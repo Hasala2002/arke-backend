958471633cf7c3d2171891364fa1fba82c881a54
Hasala2002 <hasala.h@hotmail.com>
2022-12-27 04:54:11 +0000

    server backup

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const app = express();
 import * as dotenv from 'dotenv' 
 dotenv.config()
 
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 export const client = {
   user: process.env.pocketbase_user,
   password: process.env.pocketbase_password,
 }
 
-import http from 'http'
+import https from 'https'
 
 import { Server } from 'socket.io'
 
@@ -27,16 +36,16 @@ console.log(pb.authStore.isValid);
 
 app.use(cors());
 
-// const httpsOptions = {
-//   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//   cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-// }
+ const httpsOptions = {
+   key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+   cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+ }
 
-const server = http.createServer(app);
+const server = https.createServer(httpsOptions,app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173','https://arke.vercel.app'],
+    origin: ['http://localhost:5173','https://arke.vercel.app','https://app.arkechat.live'],
     methods: ["GET", "POST"],
   },
 });
